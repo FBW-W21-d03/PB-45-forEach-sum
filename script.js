baee96e41a7e42daa532f4f834eeb8798a9eb1b0
Vens <v.kolev@alfatrex.com>
2022-01-10 11:53:12 +0100

    vens

---
+++ script.js
@@ -0,0 +1,10 @@
+const zahlen = [3066, 3189, 4001, 200, 0];
+
+let counter = 0;
+
+zahlen.forEach(zahlen => {
+   counter = counter + zahlen;
+});
+
+console.log(counter);
+//console.log(Math.sum(zahlen));
