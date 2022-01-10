5f9af4b803e2cf152181e4b313d874211c57db98
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-01-10 11:32:08 +0100

    forEach

---
+++ index.js
@@ -0,0 +1,9 @@
+const zahlen = [
+    3066, 3189, 4001, 200 , 0
+];
+total = [];
+zahlen.forEach(function(namber){
+      total += namber;
+    });
+
+    console.log("Die Summe ller ahlen ist :", total);
