3a43b346d210a6907e7f61b94672ef6fe85f2ed4
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-10 11:42:47 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,13 @@
+const zahlen = [
+    3066, 3189, 4001, 200 , 0
+];
+let initialwert = 0;
+
+zahlen.forEach(
+    (zahl) =>{
+        console.log(zahlen);
+      initialwert = initialwert  + zahl
+    }
+    
+);
+console.log(initialwert);
