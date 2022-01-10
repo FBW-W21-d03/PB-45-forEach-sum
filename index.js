82e15392a676cc14faf1f418feee7294e397286f
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-10 11:39:26 +0100

    Meine BMI Projekt

---
+++ index.js
@@ -0,0 +1,14 @@
+// AUFGABE TEIL-1
+const numbers = [3066, 3189, 4001, 200, 0];
+
+//  AUFGABE TEIL - 2
+let counter = 0;
+numbers.forEach(
+    (number) => {
+
+        counter = counter + number;
+    }
+);
+
+//   AUFGABE TEIL - 3
+console.log(`${counter}`);
