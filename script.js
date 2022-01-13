d3bd4a5d74c1f03ef4e235af9e4bcc82ae33594a
ShiinyFlakes <mago_elm@hotmail.com>
2022-01-13 13:35:20 +0100

    Lösung

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-/*const zahlen = [3066, 3189, 4001, 200, 0];
+const zahlen = [3066, 3189, 4001, 200, 0];
 let reducer = (ersteWert, zweiterWert) => ersteWert + zweiterWert
 zahlen.forEach(
     (jahrgang) =>{
         console.log(zahlen.reduce(reducer));
     }
-);*/
+);
 
 const array1 = [3066, 3189, 4001, 200, 0];
 let counter = 0;
@@ -15,3 +15,11 @@ array1.forEach(
     }
 )
 console.log(counter)
+
+/*function isBigEnough(value) {
+    return value >= 100;
+  }
+  
+  let filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
+  // filtered is [12, 130, 44]
+  console.log(filtered)*/
