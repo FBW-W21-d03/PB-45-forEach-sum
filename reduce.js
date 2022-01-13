2afb28caf5ed7a18e0670925236d2e9ce981bb5e
sprotte <metatron1972@live.de>
2022-01-13 14:52:46 +0100

    sprotte

--- reduce.js
+++ reduce.js
@@ -1,5 +1,8 @@
+/*
+! Wenn use strict gesetzt ist, müssen Variablen deklariert werden. */
 "use strict";
-
+/*
+!  Die const -Deklaration erstellt eine Konstante.*/
 const employees = [
   "Sammy",
   "Oscar",
@@ -12,8 +15,10 @@ const employees = [
   "James",
   "Amelia",
 ];
-
+/*
+! Die reduce()-Methode reduziert ein Array auf einen einzigen Wert, indem es jeweils zwei Elemente (von links nach rechts) durch eine gegebene Funktion reduziert.*/
 const reducer = (previousValue, currentValue) =>
   previousValue + " , " + currentValue;
-
+/*
+! gibt eine Nachricht als String mit Variablen in die Consolen. */
 console.log(employees.reduce(reducer));
