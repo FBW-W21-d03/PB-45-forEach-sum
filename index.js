6b70f18bdddcfeb7f060d39ac2a67763b280eee5
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-13 13:37:36 +0100

    Die Lösung wird mit Verwendung von Reduce hinzugefügt

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-// AUFGABE TEIL-1
-const numbers = [3066, 3189, 4001, 200, 0];
+//  AUFGABE TEIL-1
+ const numbers = [3066, 3189, 4001, 200, 0];
 
 
-//  AUFGABE TEIL - 2
-let counter = 0;
+//   AUFGABE TEIL - 2
+ let counter = 0;
 
-numbers.forEach(
-    (number) => {
-        counter = counter + number;
-    }
-);
+ numbers.forEach(
+     (number) => {
+         counter = counter + number;
+     }
+ );
+
+//    AUFGABE TEIL - 3
+ console.log("Counter:",`${counter}`);
 
 
-//   AUFGABE TEIL - 3
-console.log(`${counter}`);
+
+
+
+// ANDER LÖSUNG MIT VEREENDUNG VON REDUCE:
+
+const numbers2 = [3066, 3189, 4001, 200, 0];
+
+const result = numbers2.reduce((previouce, current) => previouce + current);
+
+console.log("Result ist:",`${result}`);
