102f484235706d709bea975e8f6672a108d2bd48
sham912411 <shamalhakim91@gmail.com>
2022-01-13 14:07:50 +0100

    l

--- script.js
+++ script.js
@@ -4,17 +4,8 @@ let counter = 0; // speichert Summe
 zahlen.forEach((zahl) => {
   counter += zahl;
 });
-
-<<<<<<< HEAD
-       counter = counter + plus;
-    }
-);
-console.log(counter);
 const zahlen = [
     3066, 3189, 4001, 200, 0];
     const sum =zahlen.reduce((a,b)=>a+b);
 console.log("Die Summe aller Zahlen ist :",sum);
 ;
-=======
-console.log("Gesamtsumme:", counter);
->>>>>>> 92f0761d9613c55e8352f4fc48bae3b741f2a185
