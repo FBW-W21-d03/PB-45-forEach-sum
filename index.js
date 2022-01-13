4a55e7f486d7e0084695078d700e97246f83ea7e
Mazen88cdi <89069682+Mazen88cdi@users.noreply.github.com>
2022-01-13 13:46:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const Zahlen = [3066, 3189, 4001, 200, 0];
+// const Zahlen = [3066, 3189, 4001, 200, 0];
 
-let counter = 0;
+// let counter = 0;
 
 
-Zahlen.forEach(
-    (Zahlen) => {
-        counter = counter + Zahlen;
-    }
-);
+// Zahlen.forEach(
+//     (Zahlen) => {
+//         counter = counter + Zahlen;
+//     }
+// );
 
+
+
+// console.log(counter)
+
+
+
+const zahlen = [3066, 3189, 4001, 200, 0];
+
+const counter = zahlen.reduce((v , j) => v + j);
 
 
 console.log(counter)
